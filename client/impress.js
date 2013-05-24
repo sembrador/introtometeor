3303b939873ab7ec1e103f218d01b0ace8944d7f
steeve cannon <stephentcannon@gmail.com>
2013-05-24 16:21:38 -0400

    commented out case: 9 line 714 in impress.js

--- client/impress.js
+++ client/impress.js
@@ -711,7 +711,7 @@ Meteor.startup(function() {
                     case 38: // up
                              api.prev();
                              break;
-                    case 9:  // tab
+                    //case 9:  // tab
                     case 32: // space
                     case 34: // pg down
                     case 39: // right
